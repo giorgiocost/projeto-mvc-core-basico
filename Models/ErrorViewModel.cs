cfe1f289d940d2e8baa681a1af0a34ce2768026c
Giorgio Costa <gegiorgio1991@gmail.com>
2020-05-09 22:11:41 -0300

    projeto iniciado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projeto_mvc_core_basico.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
